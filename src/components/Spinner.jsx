3122becf6ba950d55654a30eb3b48f788e7ada3a
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-03-26 19:37:25 +0200

    update

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import './Spinner.css';
 const Spinner = () => {
 
     return (
-        <div className="lds-ring">
+        <div className="lds-ripple">
             <div></div>
             <div></div>
             <div></div>
