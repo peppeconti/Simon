eaf6b849a9be6f3d73308cff5223d7b65e89e09e
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-05 19:46:50 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import Simon from './components/Simon';
-import './App.css';
+import { useState } from "react";
+import Simon from "./components/Simon";
+import InfoButton from "./components/InfoButton";
+import Infos from "./components/Infos";
+import { useMediaQuery } from "./hooks/useMediaQuery";
+import "./App.css";
 
 function App() {
+  const [infos, setInfos] = useState(false);
+
+  const mobile = useMediaQuery("(max-width: 1024px)");
+
+  let height;
+
+  if (mobile) {
+    height = `${window.outerHeight}px`;
+  } else {
+    height = "100vh";
+  }
+
+  const AppHeight = {
+    height: height,
+  };
+
   return (
-    <div className='App'>
+    <div style={AppHeight} className="App">
       <Simon />
+      {infos && <Infos setInfos={setInfos} />}
+      <InfoButton setInfos={setInfos} />
     </div>
   );
 }
