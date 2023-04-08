8f2c534f742ea50ac65f2f761a4a0a135dd2303a
peppeconti <46853326+peppeconti@users.noreply.github.com>
2023-04-08 11:15:37 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import Simon from "./components/Simon";
 import InfoButton from "./components/InfoButton";
 import Infos from "./components/Infos";
-import { useInnerHeight } from "./hooks/useInnerHeight";
+//import { useInnerHeight } from "./hooks/useInnerHeight";
 import "./App.css";
 
 function App() {
   const [infos, setInfos] = useState(false);
-  const [innerHeight] = useInnerHeight("(max-width: 1024px)");
+  const header = useRef();
+  //const [innerHeight] = useInnerHeight("(max-width: 1024px)");
+
+  useEffect(() => {
+    const appHeight = () => {
+      header.current.style.setProperty("--app-height", `${window.innerHeight}px`);
+    };
+
+    window.addEventListener("resize", appHeight);
+    appHeight();
+    return () => window.removeEventListener("resize", appHeight)
+  }, []);
 
   return (
-    <div style={innerHeight} className="App">
+    <div ref={header} className="App">
       <Simon />
       {infos && <Infos setInfos={setInfos} />}
       <InfoButton setInfos={setInfos} />
